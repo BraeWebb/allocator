b46bbb7c09fda7e4cf57c48074d44101d660caef
BraeWebb <email@braewebb.com>
2017-05-07 23:07:33 +1000

    Added Task Sheet Test
    Added a simple test for the task sheet example.

--- assignment2/tests/planner/AllocatorTest.java
+++ assignment2/tests/planner/AllocatorTest.java
@@ -14,8 +14,40 @@ import java.util.*;
 public class AllocatorTest {
 
     @Test(timeout = 5000)
-    public void test() {
-        Assert.fail();
-    }
+    public void task_sheet_test() {
+        // checks the assignment sheet example
+        Event e0 = new Event("e0", 10);
+        Event e1 = new Event("e1", 7);
+        Event e2 = new Event("e2", 5);
 
+        ArrayList<Event> events = new ArrayList<Event>();
+        events.add(e0);
+        events.add(e1);
+        events.add(e2);
+
+        ArrayList<Venue> venues;
+        try {
+            venues = (ArrayList) VenueReader.read("task_sheet_venues.txt");
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+            return;
+        }
+
+        Set<Map<Event, Venue>> expected = new HashSet<>();
+
+        HashMap<Event, Venue> map1 = new HashMap<>();
+        map1.put(e0, venues.get(0));
+        map1.put(e1, venues.get(3));
+        map1.put(e2, venues.get(2));
+
+        HashMap<Event, Venue> map2 = new HashMap<>();
+        map2.put(e0, venues.get(2));
+        map2.put(e1, venues.get(3));
+        map2.put(e2, venues.get(0));
+
+        expected.add(map1);
+        expected.add(map2);
+
+        Assert.assertTrue(expected.contains(Allocator.allocate(events, venues)));
+    }
 }
